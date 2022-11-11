d808a88535356fbaaeb17c5afc850e21852218a0
halilatilla <atillahalil0@gmail.com>
2022-11-11 18:12:46 +0300

    feat: add types

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,33 @@ import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
 
-const config = {
-  input: 'src/index.tsx',
-  output: {
-    file: 'dist/index.esm.js',
-    format: 'esm',
+const config = [
+  {
+    input: 'src/index.tsx',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+    external: [/@babel\/runtime/, 'react'],
+    plugins: [
+      babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
+      filesize(),
+      typescript({
+        clean: true,
+        rollupCommonJSResolveHack: false,
+        outDir: 'types',
+        useTsconfigDeclarationDir: true,
+      }),
+      terser(),
+    ],
   },
-  external: [/@babel\/runtime/, 'react'],
-  plugins: [
-    babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
-    filesize(),
-    typescript({
-      clean: true,
-      rollupCommonJSResolveHack: false,
-    }),
-    terser(),
-  ],
-};
+  {
+    input: './types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
 
 export default config;
