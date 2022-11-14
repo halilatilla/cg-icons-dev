9a13dad197221e629aef853c048ffa1a45fa9aee
halilatilla <atillahalil0@gmail.com>
2022-11-14 15:13:59 +0300

    fix: sdaf

---
+++ dist/Dart.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+interface SVGRProps {
+    title?: string;
+    titleId?: string;
+}
+declare const Memo: React.MemoExoticComponent<({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => JSX.Element>;
+export default Memo;
--- rollup.config.js
+++ rollup.config.js
@@ -2,32 +2,23 @@ import { babel } from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import dts from 'rollup-plugin-dts';
 
-const config = [
-  {
-    input: 'src/index.tsx',
-    output: {
-      file: 'dist/index.js',
-      format: 'esm',
-    },
-    external: [/@babel\/runtime/, 'react'],
-    plugins: [
-      babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
-      filesize(),
-      typescript({
-        clean: true,
-        outDir: 'types',
-        useTsconfigDeclarationDir: true,
-      }),
-      terser(),
-    ],
+const config = {
+  input: 'src/index.tsx',
+  output: {
+    file: 'dist/index.esm.js',
+    format: 'esm',
   },
-  {
-    input: './types/index.d.ts',
-    output: [{ file: 'dist/types.d.ts', format: 'esm' }],
-    plugins: [dts()],
-  },
-];
+  external: [/@babel\/runtime/, 'react'],
+  plugins: [
+    babel({ babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime'] }),
+    filesize(),
+    typescript({
+      clean: true,
+      rollupCommonJSResolveHack: false,
+    }),
+    terser(),
+  ],
+};
 
 export default config;
